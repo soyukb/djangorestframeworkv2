c35cab44301909c2ca17d9c35d29ad298df829de
soyukb <soyukoba@gmail.com>
2023-12-24 12:02:46 +0900

    コメントを入れる

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,22 +1,44 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
-@api_view()
+@api_view(['GET','POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    print(movies)
-    print(serializer.data)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        
 
-@api_view()
+@api_view(['GET','PUT','DELETE'])
 def movie_detail(request, pk):
-    movie = Movie.objects.get(pk = pk)
-    serializer = MovieSerializer(movie)
-    print(movie)
-    print(serializer)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk = pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk = pk)
+        serializer = MovieSerializer(movie,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk = pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+         
     
 
